5ec8beca4409fa42562fa8344f162cb2d325bfed
bricorne <bricepoitevineau@gmail.com>
2023-05-17 06:37:24 -0700

    task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || !node->parent->parent || !node->parent)
+	if (node == NULL || node->parent->parent == NULL || node->parent == NULL)
 		return (NULL);
 
 	if (node->parent == node->parent->parent->left)
